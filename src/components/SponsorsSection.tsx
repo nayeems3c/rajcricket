81c71fadafd23b5c41647c29bc1925378a90dad6
nayeems3c <aurthohinramim@gmail.com>
2025-10-11 03:02:36 +0600

    Fix sponsor logos - Convert absolute paths to proper imports for production build

--- src/components/SponsorsSection.tsx
+++ src/components/SponsorsSection.tsx
@@ -2,37 +2,36 @@ import { Button } from "./ui/button";
 import { motion, useScroll, useTransform } from "motion/react";
 import { useRef } from "react";
 
+// Import sponsor logos
+import logo1 from "../assets/3d76cd4a07b9dfacff685b08fde19931262ce844.png";
+import logo2 from "../assets/37ee764b5e147db627aaf0a621b6f01118af67c2.png";
+import logo3 from "../assets/ccd8d9290c08d4d724e45556c737044103e98714.png";
+import logo4 from "../assets/2568c957f78ff84072d98d4de60aeca8df38da88.png";
+import logo5 from "../assets/2a1c9f5647e0cd2652bf1ffb96912ed48c91b1e8.png";
+import logo6 from "../assets/16c654af10d09a2bd68f720f97162540e640d579.png";
+import logo7 from "../assets/063048d1a07be8d0b5ccbe671778f3f6ac386fcb.png";
+import logo8 from "../assets/0ba39a0b61632e55cdad246d51c1f2e2ee394bb0.png";
+import logo9 from "../assets/c771c83485d32984c776e00efb1c9c5ba8d36eb4.png";
+import logo10 from "../assets/dda05db18d64bb5cdde539267cee749bfb5dc927.png";
+import logo11 from "../assets/d8caf45e1e0c9302c8574e6dbd723bc1a1806828.png";
+import logo12 from "../assets/863ddd9f93f407206d8c26b99ef7b00d7f4d7372.png";
+import logo13 from "../assets/6fac12f1edb5876f92af02970db8f9a34e34d358.png";
+import logo14 from "../assets/270e3eee8af7654ab2eaf9195ab99617c59222d7.png";
+import logo15 from "../assets/e9e068918750188554956d1d188c03b18fba7939.png";
+import logo16 from "../assets/a7d5a695ae2a8ccd1f346816b7c029021a0ef891.png";
+import logo17 from "../assets/fa10be856beae9c9b6959851dc2994c2179e1a7b.png";
+import logo18 from "../assets/97bb09746df5687bc55a4cd40c4056ec6204ecd9.png";
+import logo19 from "../assets/540163c9be50d8915138f392162ec232a1b08132.png";
+import logo20 from "../assets/0044c508758929d0a077afdd18c71d94f73ff05f.png";
+import logo21 from "../assets/ea39ee05baf2459b364f42f2613be80a1d71cec8.png";
+import logo22 from "../assets/dc39357ad5954ccae3332faa001344976cdcbca2.png";
+
 // Using existing assets in the assets folder
-const logoRow1 = [
-  "/src/assets/3d76cd4a07b9dfacff685b08fde19931262ce844.png",
-  "/src/assets/37ee764b5e147db627aaf0a621b6f01118af67c2.png",
-  "/src/assets/ccd8d9290c08d4d724e45556c737044103e98714.png",
-  "/src/assets/2568c957f78ff84072d98d4de60aeca8df38da88.png",
-  "/src/assets/2a1c9f5647e0cd2652bf1ffb96912ed48c91b1e8.png",
-  "/src/assets/16c654af10d09a2bd68f720f97162540e640d579.png",
-  "/src/assets/063048d1a07be8d0b5ccbe671778f3f6ac386fcb.png",
-  "/src/assets/0ba39a0b61632e55cdad246d51c1f2e2ee394bb0.png",
-];
+const logoRow1 = [logo1, logo2, logo3, logo4, logo5, logo6, logo7, logo8];
 
-const logoRow2 = [
-  "/src/assets/c771c83485d32984c776e00efb1c9c5ba8d36eb4.png",
-  "/src/assets/dda05db18d64bb5cdde539267cee749bfb5dc927.png",
-  "/src/assets/d8caf45e1e0c9302c8574e6dbd723bc1a1806828.png",
-  "/src/assets/863ddd9f93f407206d8c26b99ef7b00d7f4d7372.png",
-  "/src/assets/6fac12f1edb5876f92af02970db8f9a34e34d358.png",
-  "/src/assets/270e3eee8af7654ab2eaf9195ab99617c59222d7.png",
-  "/src/assets/e9e068918750188554956d1d188c03b18fba7939.png",
-  "/src/assets/a7d5a695ae2a8ccd1f346816b7c029021a0ef891.png",
-];
+const logoRow2 = [logo9, logo10, logo11, logo12, logo13, logo14, logo15, logo16];
 
-const logoRow3 = [
-  "/src/assets/fa10be856beae9c9b6959851dc2994c2179e1a7b.png",
-  "/src/assets/97bb09746df5687bc55a4cd40c4056ec6204ecd9.png",
-  "/src/assets/540163c9be50d8915138f392162ec232a1b08132.png",
-  "/src/assets/0044c508758929d0a077afdd18c71d94f73ff05f.png",
-  "/src/assets/ea39ee05baf2459b364f42f2613be80a1d71cec8.png",
-  "/src/assets/dc39357ad5954ccae3332faa001344976cdcbca2.png",
-];
+const logoRow3 = [logo17, logo18, logo19, logo20, logo21, logo22];
 
 export function SponsorsSection() {
   return (
